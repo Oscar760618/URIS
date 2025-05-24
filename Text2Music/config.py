37873ae2787b0d6fc8d4322f70c0f4f5c2ebe3b8
LUO TIANYOU <60978418+Oscar760618@users.noreply.github.com>
2025-05-24 16:55:33 +0800

    Add files via upload

---
+++ Text2Music/config.py
@@ -0,0 +1,19 @@
+TEXT_DATA_ROOT = 
+MUSE_ROOT = 
+
+TEST_MUSE_POS_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_music_pos_samples.txt"
+TEST_MUSE_NEG_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_music_neg_samples.txt"
+TEST_TEXT_POS_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_text_pos_samples.txt"
+TEST_TEXT_NEG_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_text_neg_samples.txt"
+
+TRAIN_MUSE_POS_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_music_pos_samples.txt"
+TRAIN_MUSE_NEG_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_music_pos_samples.txt"
+TRAIN_TEXT_POS_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_text_pos_samples.txt"
+TRAIN_TEXT_NEG_ROOT = "D:/PolyU\URIS/Part2_projects/WEMOM_V1/Text2Music/Samples/test_text_pos_samples.txt"
+
+
+CL_DIM = 256
+MID_DIM = 512
+bw = 30
+init_lr = 0.0001
+EPOCH = 5
